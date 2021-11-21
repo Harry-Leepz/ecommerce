d4241a9a56e9fc73774453064f61bdb36de4ce5d
Harry <hdhillon478@gmail.com>
2021-11-21 18:30:50 +0000

    Build Card component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
-function Product() {
-  return <div></div>;
+function Product({ product }) {
+  return (
+    <Card className='my-3 p-3 rounded'>
+      {/* Image */}
+      <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} />
+      </a>
+
+      <Card.Body>
+        {/* Name */}
+        <a href={`/product/${product._id}`}>
+          <Card.Title as='div'>
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+
+        {/* Reviews */}
+        <Card.Text as='div'>
+          <div className='my-3'>
+            {product.rating} from {product.numReviews} reviews
+          </div>
+        </Card.Text>
+
+        {/* Price */}
+        <Card.Text as='h3'>${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default Product;
