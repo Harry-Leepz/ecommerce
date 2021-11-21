61689bf8dae8f1b6aa33e40a19b477cc548f7f09
Harry <hdhillon478@gmail.com>
2021-11-21 17:47:42 +0000

    Add images folder and Add products.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-        <Container>
+        <Container fluid>
           <Navbar.Brand href='/'>ProShop</Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
