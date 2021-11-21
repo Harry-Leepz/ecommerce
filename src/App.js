234bf1abfc1a3ef86cb8623337b57837545a17c3
Harry <hdhillon478@gmail.com>
2021-11-21 12:54:03 +0000

    Create Footer component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
+      <Header />
       My App
+      <Footer />
     </div>
   );
 }
