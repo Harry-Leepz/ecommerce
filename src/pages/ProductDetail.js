d7d4f64fc61e18230f4200053fad76e92a54fa67
Harry <hdhillon478@gmail.com>
2021-11-23 14:02:22 +0000

    Add product image to ProductDetail page

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -7,7 +7,19 @@ import products from "../products";
 function ProductDetail({ match }) {
   // Check to confirm the product id matches the parameter passed in the url
   const product = products.find((p) => p._id === match.params.id);
-  return <div>{product.name}</div>;
+  return (
+    <div>
+      <Link to='/' className='btn btn-success my-3 rounded-0'>
+        Go Back
+      </Link>
+      <Row>
+        {/* Product Image */}
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default ProductDetail;
