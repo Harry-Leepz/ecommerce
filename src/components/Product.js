2e92df69084d68e628762d7cac9d3e03aab088b9
Harry <hdhillon478@gmail.com>
2021-11-23 10:31:46 +0000

    Change <a> tags to Links in Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Rating from "./Rating";
 import { Card } from "react-bootstrap";
 
@@ -6,17 +7,17 @@ function Product({ product }) {
   return (
     <Card className='my-3 p-3 rounded'>
       {/* Image */}
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} />
-      </a>
+      </Link>
 
       <Card.Body>
         {/* Name */}
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as='div'>
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
 
         {/* Reviews */}
         <Card.Text as='div'>
