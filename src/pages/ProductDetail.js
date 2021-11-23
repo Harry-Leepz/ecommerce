2e7ab4d76232dd9a75ce77f77084c082d04a9f47
Harry <hdhillon478@gmail.com>
2021-11-23 13:54:54 +0000

    Add imports to ProductDetail.js

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../products";
 
-function ProductDetail() {
-  return <div>product</div>;
+function ProductDetail({ match }) {
+  // Check to confirm the product id matches the parameter passed in the url
+  const product = products.find((p) => p._id === match.params.id);
+  return <div>{product.name}</div>;
 }
 
 export default ProductDetail;
