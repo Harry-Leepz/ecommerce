1da5581d15f9c7f8aa174092570a54032062ad51
Harry <hdhillon478@gmail.com>
2021-11-23 10:28:17 +0000

    Create ProductDetail component +  route in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
+import ProductDetail from "./pages/ProductDetail";
 import Footer from "./components/Footer";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <main className='py-3'>
         <Container>
           <Route exact path='/' component={HomePage} />
+          <Route path='/product/:id' component={ProductDetail} />
         </Container>
       </main>
       <Footer />
